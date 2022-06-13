b60a87a8bd6b762c0ce0422f7018b7caf0d98c9d
Isaac Dutton <k1p3psyygojb@nowbuzzoff.com>
2022-06-13 14:22:57 -0400

    refactored after solution

--- Medium/6-Custom_Set/custom_set.rb
+++ Medium/6-Custom_Set/custom_set.rb
@@ -1,13 +1,12 @@
 class CustomSet
   include Comparable
-  attr_reader :objects
 
   def initialize(objects=[])
-    @objects = objects
+    @objects = objects.uniq
   end
 
   def <=>(other_set)
-    objects.uniq.sort <=> other_set.objects.uniq.sort
+    objects.sort <=> other_set.objects.sort
   end
 
   def empty?
@@ -19,11 +18,11 @@ class CustomSet
   end
 
   def subset?(other_set)
-    objects.all? { |obj| other_set.objects.include?(obj) }
+    objects.all? { |obj| other_set.contains?(obj) }
   end
 
   def disjoint?(other_set)  
-    !objects.any? { |obj| other_set.objects.include?(obj) }
+    !objects.any? { |obj| other_set.contains?(obj) }
   end
 
   def eql?(other_set)
@@ -31,22 +30,26 @@ class CustomSet
   end
 
   def add(obj)
-    objects.push(obj)
+    objects.push(obj) unless contains?(obj)
     self
   end
 
   def intersection(other_set)
-    new_objects = objects.select { |obj| other_set.objects.include?(obj) }
+    new_objects = objects.select { |obj| other_set.contains?(obj) }
     CustomSet.new(new_objects)
   end
 
   def difference(other_set)
-    new_objects = objects.reject { |obj| other_set.objects.include?(obj) }
+    new_objects = objects.reject { |obj| other_set.contains?(obj) }
     CustomSet.new(new_objects)
   end
   
   def union(other_set)
     new_objects = objects + other_set.objects
-    CustomSet.new(new_objects.uniq)
+    CustomSet.new(new_objects)
   end
+
+  protected
+
+  attr_reader :objects
 end
